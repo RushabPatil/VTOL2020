eb12c801b015ccbcee42d85a8468be3bb85c7f94
bmferrarotti <53305682+bmferrarotti@users.noreply.github.com>
2019-09-30 13:30:22 -0400

    Add files via upload
    fixed motor_find function

--- Toolbox_MQP_Brian_2/motor_find.m
+++ Toolbox_MQP_Brian_2/motor_find.m
@@ -1,13 +1,19 @@
 % Aerospace MQP VTOL UAV Optimizer motor_find
-function motor_info=motor_find(motor_input);
-%m_info=[Current (A),Power(W),RPM,Efficency (G/W),Weight (Kg), Thrust(Kg)]
-%per single motor. %Ideally load in spreadsheet and index into it. 
-m_info=readtable('C:\Users\eve\Documents\WPI\MQP\Toolbox_MQP_Brian_2\Motor_Comparison.csv','ReadVariableNames',false);
-%{ 
- BACK UP HARD CODED MOTOR DATA
-m_info=
-%}
-motor_options=['MN2212KV780V2.0','EMAXMT3510','admiral-gp5-4220-770kv','LHILHM029EMAXCF2822KV1200','DYSD3536910Kv1999kv','EMAXMT2213-935KV','EMAXMT4008-600KV','EMAXMT3110-700KV','EMAXMT3510-600KV','ScorpionM3011-760KV','ScorpionM4010-400KV'];
-index=find(strcmp(motor_options,motor_input)~=0,1);
-motor_info=m_info{index,:};
+function [motor_info, motor_mass] = motor_find(motor_input);
+string = motor_input + '.txt';
+
+% open the corresponding .txt file
+fileID = fopen(string,'r');
+
+% convert the contents of the .txt file to an Nx2 matrix
+formatSpec = '%f %f %f %f %f %f';
+size_points = [5 Inf];
+motor_info_interim = fscanf(fileID,formatSpec,size_points)';
+% leave out the last column as this only contains repeated referencess to
+% the motor mass
+
+g = 9.81;   % gravity, m s^-2
+% columns of useful stuff: [power, thrust(N)], attaching [0, 0] to the top
+motor_info = motor_info_interim(:,1:end-1);
+motor_mass = motor_info_interim(1,end);
 end
